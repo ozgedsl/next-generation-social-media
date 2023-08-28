edae4dd58b48cd4be51c66e27847fe6d78d5acd0
ozgenur.dasli <ozgenur.dasli@partner.yildizholding.com.tr>
2023-08-28 16:34:07 +0300

    first commit

---
+++ src/main/java/com/app/nextgenerationsocialmedia/NextGenerationSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.app.nextgenerationsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGenerationSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextGenerationSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/nextgenerationsocialmedia/NextGenerationSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.nextgenerationsocialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGenerationSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
